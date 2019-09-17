39d10ec63451e2a9fcecbb5d0200266e861f1df1
Kaitlyn Clark <kcla0004@student.monash.edu>
2019-09-17 12:49:54 +1000

    Sell action added

---
+++ src/game/SellAction.java
@@ -0,0 +1,30 @@
+package game;
+
+import edu.monash.fit2099.engine.Action;
+import edu.monash.fit2099.engine.Actor;
+import edu.monash.fit2099.engine.GameMap;
+import edu.monash.fit2099.engine.Item;
+
+public class SellAction extends Action
+{
+    private Item sellItem;
+
+    public SellAction(Item sellItem)
+    {
+        this.sellItem = sellItem;
+    }
+
+    @Override
+    public String execute(Actor actor, GameMap map)
+    {
+        ((Player) actor).addMoney(((Sellable) sellItem).getValue());
+        actor.removeItemFromInventory(sellItem);
+        return sellItem.toString() + " sold by player for " + ((Sellable) sellItem).getValue();
+    }
+
+    @Override
+    public String menuDescription(Actor actor)
+    {
+        return "Sell " + sellItem.toString() + " for " + ((Sellable) sellItem).getValue();
+    }
+}
