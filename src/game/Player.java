b9636b930625017393e235fd6d1cdcff5253fb75
Brian Li Ong <bong0005@student.monash.edu>
2019-09-24 11:12:20 +1000

    Added getHealthy method into Player class

--- src/game/Player.java
+++ src/game/Player.java
@@ -10,7 +10,8 @@ import edu.monash.fit2099.engine.Menu;
 /**
  * Class representing the Player.
  */
-public class Player extends Actor {
+public class Player extends Actor
+{
 
 	private Menu menu = new Menu();
 	private int wallet = 0;
@@ -22,12 +23,14 @@ public class Player extends Actor {
 	 * @param displayChar Character to represent the player in the UI
 	 * @param hitPoints   Player's starting number of hitpoints
 	 */
-	public Player(String name, char displayChar, int hitPoints) {
+	public Player(String name, char displayChar, int hitPoints)
+	{
 		super(name, displayChar, hitPoints);
 	}
 
 	@Override
-	public Action playTurn(Actions actions, Action lastAction, GameMap map, Display display) {
+	public Action playTurn(Actions actions, Action lastAction, GameMap map, Display display)
+	{
 		// Handle multi-turn Actions
 		if (lastAction.getNextAction() != null)
 			return lastAction.getNextAction();
@@ -41,12 +44,20 @@ public class Player extends Actor {
 
 	public String removeMoney(int value)
 	{
-		if (value < this.wallet)
-		{
+		if (value < this.wallet) {
 			return "Not enough money to complete purchase";
 		}
 
 		this.wallet -= value;
 		return null;
+	}
+
+	@Override
+	public Boolean isHealthy()
+	{
+		if (this.hitPoints < this.maxHitPoints) {
+			return false;
+		}
+		return true;
 	}
 }
