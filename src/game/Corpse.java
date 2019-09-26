a8a2377c9a2dd2790a3fd064c235fc4c5e1b86d4
Brian Li Ong <bong0005@student.monash.edu>
2019-09-26 14:29:06 +1000

    Modified attack action to support added corpse type, changed corpse type to actor instead of dinosaur. Added javadoc comments to corpse class.

--- src/game/Corpse.java
+++ src/game/Corpse.java
@@ -1,33 +1,49 @@
 package game;
 
+import edu.monash.fit2099.engine.Actor;
 import edu.monash.fit2099.engine.Item;
 
+/**
+ * A class that represents corpses
+ */
 public class Corpse extends Item implements Sellable, Edible
 {
-    Dinosaur type;
+    Actor type;
 
     /***
      * Constructor.
-     *  @param name the name of this Item
+     *  @param name the name of the corpse
      */
-    public Corpse(String name, Dinosaur type)
+    public Corpse(String name, Actor type)
     {
         super(name, 'x', true);
         this.type = type;
     }
 
+    /**
+     * Gets the number of hunger points restored when eaten
+     * @return hunger points restored when eaten
+     */
     @Override
     public int getFoodValue()
     {
         return 50;
     }
 
+    /**
+     * Gets the type of food that corpse is
+     * @return enum food type of corpse
+     */
     @Override
     public FoodType getFoodType()
     {
         return FoodType.MEAT;
     }
 
+    /**
+     * Gets the monetary value of the corpse for buying and selling
+     * @return monetary value of corpse
+     */
     @Override
     public int getValue()
     {
