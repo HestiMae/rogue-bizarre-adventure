5b1d99d0eff69d7a87442c28f486128cbf6e5971
Brian Li Ong <bong0005@student.monash.edu>
2019-10-09 13:02:48 +1100

    Fixed tag action bug where tag is removed when unsuccessful

--- src/game/Protoceratops.java
+++ src/game/Protoceratops.java
@@ -14,19 +14,14 @@ public class Protoceratops extends Dinosaur
     private static final int HIT_POINTS = 100; //max HP for protoceratops
     private static final char DISPLAY_CHAR = 'd'; //display character for protoceratops
     private static final int ADULT_AGE = 20; //the age in turns baby protoceratops will become adults
-    private static final int MAX_HUNGER = 50; //the max hunger level for protoceratops
+    private static final int MAX_HUNGER = 50; //the max metabolise level for protoceratops
+    private static final int HUNGRY_AT_LEVEL = MAX_HUNGER - 10;
     private static final int HATCH_TIME = 10; //the time a protoceratops egg takes to hatch
-
-    public static int getBreedHunger()
-    {
-        return BREED_HUNGER;
-    }
-
-    private static final int BREED_HUNGER = 40; //the hunger level protoceratops require to breed
-    private static final int START_HUNGER_LEVEL = 50; //the hunger level of a new protoceratops
-    private static final int HUNGER_THRESHOLD = 20; //the hunger level when the dinosaur is considered "hungry" - players are warned once it reaches this level
-    private static final int HUNGER_LOSS = 2; //the hunger level loss per turn
-    private static final int HUNGER_DAMAGE = 10; //the HP damage per turn the hunger level is at 0
+    private static final int BREED_HUNGER = 40; //the metabolise level protoceratops require to breed
+    private static final int START_HUNGER_LEVEL = 30; //the metabolise level of a new protoceratops
+    private static final int HUNGER_THRESHOLD = 20; //the metabolise level when the dinosaur is considered "hungry" - players are warned once it reaches this level
+    private static final int HUNGER_LOSS = 2; //the metabolise level loss per turn
+    private static final int HUNGER_DAMAGE = 10; //the HP damage per turn the metabolise level is at 0
     private static final int COST = 100; //The monetary value of a protoceratops
     private static final int FOOD_VALUE = 30; //the food value of a protoceratops
 
@@ -39,8 +34,13 @@ public class Protoceratops extends Dinosaur
     {
         super(name, DISPLAY_CHAR, HIT_POINTS);
         diet.add(FoodType.PLANT);
-        this.hungerLevel = START_HUNGER_LEVEL;
+        this.foodLevel = START_HUNGER_LEVEL;
+        addSkill(PassableTerrain.LAND);
         behaviours.add(new EatBehaviour(this, diet));
+        behaviours.add(new FollowBehaviour<Dinosaur>(this, Dinosaur::isHungry,
+                Dinosaur::canEat,
+                (dinosaur, actor) -> false,
+                Dinosaur::canEat));
         behaviours.add(new WanderBehaviour());
     }
 
@@ -48,16 +48,27 @@ public class Protoceratops extends Dinosaur
     public Action playTurn(Actions actions, Action lastAction, GameMap map, Display display)
     {
         age(ADULT_AGE);
-        hunger(HUNGER_LOSS);
-        needFood(HUNGER_DAMAGE);
-        display.println(isHungry(HUNGER_THRESHOLD));
+        metabolise(HUNGER_LOSS);
+        starve(HUNGER_DAMAGE);
+        display.println(hungerStatus(HUNGER_THRESHOLD));
         return super.playTurn(actions, lastAction, map, display);
+    }
+
+    public static int getBreedHunger()
+    {
+        return BREED_HUNGER;
     }
 
     @Override
     Dinosaur copyDinosaur()
     {
-        return new Protoceratops(this.name + " Jr.");
+        return new Protoceratops(this.name);
+    }
+
+    @Override
+    Dinosaur copyDinosaur(String nameExtension)
+    {
+        return new Protoceratops(this.name + nameExtension);
     }
 
     @Override
@@ -67,7 +78,7 @@ public class Protoceratops extends Dinosaur
     }
 
     @Override
-    public int getMaxHunger()
+    public int getMaxFoodLevel()
     {
         return MAX_HUNGER;
     }
@@ -81,13 +92,13 @@ public class Protoceratops extends Dinosaur
     @Override
     public boolean canBreed()
     {
-        return this.hungerLevel > BREED_HUNGER && this.stage == DinoAge.ADULT;
+        return this.foodLevel > BREED_HUNGER && this.stage == DinoAge.ADULT;
     }
 
     @Override
-    public boolean isFull()
+    public boolean isHungry()
     {
-        return this.hungerLevel >= MAX_HUNGER - 10;
+        return this.foodLevel < HUNGRY_AT_LEVEL;
     }
 
     @Override
@@ -106,5 +117,11 @@ public class Protoceratops extends Dinosaur
     public FoodType getFoodType()
     {
         return FoodType.MEAT;
+    }
+
+    @Override
+    public Boolean isFlying()
+    {
+        return false;
     }
 }
